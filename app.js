3bcb4f5630152940f17f03bfdc064c7cf65ca334
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-11-03 05:25:53 +0300

    Change logging on server start

--- app.js
+++ app.js
@@ -34,9 +34,7 @@ app.get('/projects', (req, res) => {
 });
 
 if (app.get(env) === 'production') {
-  const server = app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server running on port ${server.address().port}`);
-  });
+  app.listen(process.env.PORT || 8080);
 } else {
   app.listen(8000);
 }
