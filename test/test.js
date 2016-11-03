cac5f06389561fe5fd14bed8649aefdf254e5d5d
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-11-03 18:41:12 +0300

    Add tests for Object.assign

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
-const assert = require('assert');
+const { assert, expect } = require('chai');
 
 
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
+      expect([1,2,3].indexOf(4)).to.equal(-1);
+    });
+  });
+});
+
+describe('Object', () => {
+  describe('assign', () => {
+    it('should modify first object', () => {
+      const object = {name: 'original', id: 0};
+      Object.assign(object, {name: 'new'});
+      expect(object.name).to.equal('new');
+    });
+
+    it('should not modify objects in operation', function () {
+      const object = {name: 'original', id: 0};
+      const fresh = Object.assign({}, object, {name: 'new'});
+      expect(fresh.name).to.equal('new');
+      expect(object.name).to.equal('original');
     });
   });
 });
