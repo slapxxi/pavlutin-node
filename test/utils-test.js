22afcef8d85d3f5eafaab5d4a76833807bffa499
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-12-13 16:39:43 +0300

    Add additional tests for generateTitle function

--- test/utils-test.js
+++ test/utils-test.js
@@ -4,9 +4,19 @@ const { generateTitle } = require('../lib/utils');
 
 describe('utils', function() {
   describe('.generateTitle()', function() {
-    it('should create a valid title', function () {
+    it('should generate a valid title', function () {
       const result = generateTitle('Base', 'First', 'Second');
       expect(result).to.equal('Base | First | Second');
+    });
+
+    it('should generate a valid title when some arguments missing', function () {
+      const result = generateTitle('Base');
+      expect(result).to.equal('Base');
+    });
+
+    it('should return an empty string when all arguments missing', function () {
+      const result = generateTitle();
+      expect(result).to.eq('');
     });
   });
 });
