e8592eaf0c0467e402f8b612763be724bb22bb4b
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-12-16 17:10:39 +0300

    Use a router for rendering static pages

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const morgan = require('morgan');
 const compression = require('compression');
 
 const config = require('./app/config');
+const pagesRouter = require('./app/routes/pages-router');
 const { pageNotFound } = require('./app/middleware/http');
-const { generateTitle } = require('./lib/utils');
 
 
 const ENV = config.env;
@@ -22,23 +22,10 @@ app.use('/css', express.static(config.outputDirs.stylesheets));
 app.use('/js', express.static(config.outputDirs.scripts));
 app.use('/img', express.static(config.outputDirs.images));
 
+app.use('/', pagesRouter);
+
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-app.get('/blog', (req, res) => {
-  const title = generateTitle(app.locals.title, 'Blog');
-  res.render('blog', {title});
-});
-
-app.get('/projects', (req, res) => {
-  const title = generateTitle(app.locals.title, 'Projects');
-  res.render('projects', {title});
-});
-
-app.get('/contact', (req, res) => {
-  const title = generateTitle(app.locals.title, 'Contact');
-  res.render('contact');
 });
 
 app.use(pageNotFound);
---
+++ app/routes/pages-router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const config = require('../config');
+const { generateTitle } = require('../../lib/utils');
+
+
+const pagesRouter = express.Router();
+const BASE_TITLE = config.locals.title;
+
+pagesRouter.get('/blog', (req, res) => {
+  const title = generateTitle(BASE_TITLE, 'Blog');
+  res.render('blog', {title});
+});
+
+pagesRouter.get('/projects', (req, res) => {
+  const title = generateTitle(BASE_TITLE, 'Projects');
+  res.render('projects', {title});
+});
+
+pagesRouter.get('/contact', (req, res) => {
+  const title = generateTitle(BASE_TITLE, 'Contact');
+  res.render('contact', {title});
+});
+
+module.exports = pagesRouter;
