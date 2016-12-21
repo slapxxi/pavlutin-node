abc0b101efa2d8ab7f42b40e7138cd3f8a44efa3
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-12-21 17:03:10 +0300

    Define Post model

---
+++ app/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const postSchema = mongoose.Schema({
+  title: {type: String, required: true, unique: true},
+  content: {type: String, required: true},
+  createdAt: {type: Date, default: Date.now}
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
