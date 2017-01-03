3e9080bd555640dc5e8ac191d5571d6f504a210c
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-01-03 17:18:35 +0300

    helpers.lorem() returns Array instead of concatenated Strings

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -5,32 +5,27 @@ const { LOREM } = helpers;
 
 describe('helpers', function() {
   describe('.lorem()', function() {
-    const expected = `<p>${LOREM}</p>`;
+    const expected = `${LOREM}`;
 
-    it('should generate dummy text', function () {
+    it('generates dummy text', function () {
       const result = helpers.lorem(1);
       expect(result).to.eq(expected);
     });
 
-    it('should generate a single paragraph without args', function () {
+    it('generates string without args', function () {
       const result = helpers.lorem();
       expect(result).to.eq(expected);
     });
 
-    it('should generate a single paragraph with invalid args', function () {
+    it('generates string with invalid args', function () {
       const result = helpers.lorem(-1);
       expect(result).to.eq(expected);
     });
 
-    it('should generate multiple paragraphs', function () {
+    it('generates array when specified', function () {
       const result = helpers.lorem(3);
-      expect(result).to.eq(expected + expected + expected);
-    });
-
-    it('wraps dummy text in specified HTML tag', function () {
-      const result = helpers.lorem(1, 'div');
-      const expected = `<div>${LOREM}</div>`;
-      expect(result).to.eq(expected);
+      expect(result.length).to.eq(3);
+      expect(result).to.contain(expected);
     });
   });
 });
