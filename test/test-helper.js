ec4d3a2f0bcb090b4f2fe9ddff874d573499c7d9
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-01-05 19:38:18 +0300

    Add test-helper.js

---
+++ test/test-helper.js
@@ -0,0 +1,10 @@
+/**
+ * Render a React component for testing.
+ * @param  {Object} component React component
+ * @return {String} Rendered component
+ */
+function renderComponent(component) {
+  return 'component';
+}
+
+module.exports = {renderComponent};
