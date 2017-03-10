618034d97045172af54f56069304efba2976dfb2
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-03-10 18:17:39 +0300

    Change formatting in Posts router

--- app/routes/posts-router.js
+++ app/routes/posts-router.js
@@ -7,12 +7,8 @@ const postsRouter = express.Router();
 postsRouter.get('/', (req, res, next) => {
   Post.find((err, posts) => {
     if (err) next(err);
-    res.json({posts});
+    return res.json({ posts });
   });
-});
-
-postsRouter.get('/:slug', (req, res, next) => {
-  console.log(req.params);
 });
 
 module.exports = postsRouter;
