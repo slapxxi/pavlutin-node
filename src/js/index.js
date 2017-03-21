07a1611118d8ef027d5bdb1cd44cb99576be36ce
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-03-21 19:31:38 +0300

    Reset header nav styles to default

--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,8 @@ import HomePage from './components/HomePage';
 import BlogPage from './components/BlogPage';
 import ProjectsPage from './components/ProjectsPage';
 import PostContainer from './components/PostContainer';
-import Menu from './components/Menu';
 import rootReducer from './reducers/root-reducer';
-import { addPost, toggleMenu } from './actions';
+import { addPost } from './actions';
 
 
 const API_URL = '/api/v1/posts';
@@ -20,15 +19,12 @@ const API_URL = '/api/v1/posts';
 const storeParams = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(rootReducer, storeParams);
 
+// Replace fetch with univsally supported API
 fetch(API_URL).then(res => res.json()).then((result) => {
   result.posts.forEach((post) => {
     store.dispatch(addPost(post));
   });
 }).catch(error => console.log('Error fetching and parsing data', error));
-
-store.subscribe(() => {
-  updateMenuAppearance(store);
-});
 
 const appProvider = (
   <Provider store={store}>
@@ -45,37 +41,8 @@ const appProvider = (
   </Provider>
 );
 
-const menuProvider = (
-  <Provider store={store}>
-    <Menu />
-  </Provider>
-);
-
 $(() => {
   $('body').addClass('js');
 
   ReactDOM.render(appProvider, document.querySelector('.app'));
-  ReactDOM.render(menuProvider, document.querySelector('.menu-container'));
-
-  // Not supported in IE < 10
-  $(window).on('resize.menu', () => {
-    const mq = window.matchMedia('(min-width: 600px)');
-    if (mq.matches) {
-      store.dispatch(toggleMenu());
-    }
-  });
 });
-
-function updateMenuAppearance(store) {
-  const { isMenuActive } = store.getState();
-  const $header = $('.header');
-  if (isMenuActive) {
-    $header.addClass('header_fullscreen');
-    $header.addClass('header_theme_inverse');
-    $header.siblings().hide();
-  } else {
-    $header.removeClass('header_fullscreen');
-    $header.removeClass('header_theme_inverse');
-    $header.siblings().show();
-  }
-}
