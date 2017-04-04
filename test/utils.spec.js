06a06fe50c94eea410ac9ffd9e1e99ff8785c5a1
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-04 14:08:58 +0300

    Rename utils test

---
+++ test/utils.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { combineClassNames, currentYear, cycle } from '../src/js/utils';
+
+describe('utils', () => {
+  describe('combineClassNames', () => {
+    it('combines class names', () => {
+      const result = combineClassNames('nav', 'active');
+      expect(result).to.eq('active nav');
+    });
+
+    it('ignores undefined values', () => {
+      const result = combineClassNames(undefined, 'active');
+      expect(result).to.eq('active');
+    });
+
+    it('ignores empty strings', () => {
+      const result = combineClassNames('', 'active');
+      expect(result).to.eq('active');
+    });
+  });
+
+  describe('currentYear', () => {
+    it('returns current year', () => {
+      const result = currentYear();
+      expect(result).to.eq(2017);
+    });
+  });
+
+  describe('cycle', () => {
+    it('returns function that cycles indefinitelly', () => {
+      const fn = cycle([1, 2, 3]);
+      expect(fn()).to.eq(1);
+      expect(fn()).to.eq(2);
+      expect(fn()).to.eq(3);
+      expect(fn()).to.eq(1);
+    });
+  });
+});
