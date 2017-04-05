2bd1ad6ad16d02da70802dc2169c7407ca22d3e3
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-05 15:26:49 +0300

    Remove unncessary routes [skip ci]

--- app/routes/blog-router.js
+++ app/routes/blog-router.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const config = require('../config');
 const Post = require('../models/post');
 const { generateTitle } = require('../../lib/utils');
@@ -11,17 +10,8 @@ const BASE_TITLE = config.locals.title;
 blogRouter.get('/', (req, res, next) => {
   const title = generateTitle(BASE_TITLE, 'Blog');
   Post.find((err, posts) => {
-    if (err) { next(err) }
-    res.render('blog', {title, posts});
-  });
-});
-
-// TODO: Find the post corresponding to the slug or display an error
-blogRouter.get('/:slug', (req, res, next) => {
-  const { slug } = req.params;
-  Post.findOne({slug}, (err, post) => {
-    if (err) { next(err) }
-    res.render('post', {post});
+    if (err) { next(err); }
+    res.render('blog', { title, posts });
   });
 });
 
