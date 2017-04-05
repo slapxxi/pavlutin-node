dbd9f3041b46986698a5fa03da33f49df7e63888
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-05 15:09:31 +0300

    Replace double quotes with single quotes in post.js [skip ci]

--- app/models/post.js
+++ app/models/post.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 
 const postSchema = mongoose.Schema({
-  title: {type: String, required: true, unique: true},
-  slug: {type: String, required: true, unique: true},
-  description: {type: String, required: true},
-  content: {type: String, required: true},
-  createdAt: {type: Date, default: Date.now}
+  title: { type: String, required: true, unique: true },
+  slug: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
