033572156ce3ca30b38aac87e2e5c4f3d1aa4db4
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-06 13:50:25 +0300

    Delete blank line [skip ci]

--- app/models/post.js
+++ app/models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const postSchema = mongoose.Schema({
   title: { type: String, required: true, unique: true },
   slug: { type: String, required: true, unique: true },
