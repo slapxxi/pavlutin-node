abe587a3b9f8cb345ab629bcae82ce51331f52e2
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-09 18:30:46 +0300

    Add tests for post content in <Post> spec

--- test/components/Post.spec.js
+++ test/components/Post.spec.js
@@ -2,12 +2,13 @@ import { expect } from 'chai';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
 import Post from '../../src/js/components/Post';
+import { singleLine } from '../test-helper';
 
 describe('<Post/>', () => {
   const post = {
     id: 0,
     title: 'Test Post',
-    content: 'empty',
+    content: '**content**',
     description: 'empty',
     author: 'bot',
     slug: 'test-post',
@@ -17,6 +18,23 @@ describe('<Post/>', () => {
   it('renders post title', () => {
     const result = shallow(<Post post={post} />);
     expect(result.find('.post__title').text()).to.eq('Test Post');
+  });
+
+  it('renders post content', () => {
+    const result = mount(<Post post={post} />);
+    expect(result.find('.post__content').text()).to.eq('content');
+  });
+
+  it('converts post content from Markdown', () => {
+    const result = shallow(<Post post={post} />);
+    const expected = singleLine`
+      <div class="post__content">
+        <div>
+          <p><strong>content</strong></p>
+        </div>
+      </div>
+    `;
+    expect(result.find('.post__content').html()).to.eq(expected);
   });
 
   it('renders post metadata', () => {
