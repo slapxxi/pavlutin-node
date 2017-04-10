839aaf554a2618b3fc2fd961ea21f4c630dc7abf
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-10 17:39:51 +0300

    Add DefinePlugin in Webpack production config [skip ci]

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,9 @@ const webpack = require('webpack');
 function config(baseConfig) {
   const conf = {
     plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      }),
       new webpack.optimize.UglifyJsPlugin(),
     ],
   };
