a6c9cbb21023f575078ec3ae360cd9116dc8e8e4
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-11 04:02:35 +0300

    Add <Icon> to <PageNotFound>

--- src/js/components/PageNotFound.js
+++ src/js/components/PageNotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Icon from 'react-fontawesome';
 import { setTitle } from '../utils';
 
 function PageNotFound() {
   setTitle('Page Not Found');
   return (
-    <div className="container">
-      <h1>404 - Page Not Found</h1>
+    <div className="container page-not-found">
+      <h1><Icon name="exclamation-triangle" /> 404 - Page Not Found</h1>
     </div>
   );
 }
