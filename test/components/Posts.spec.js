da5e854055509992103561f5134dea0ef094bec9
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-13 17:32:25 +0300

    Test passing tag to <Posts>

--- test/components/Posts.spec.js
+++ test/components/Posts.spec.js
@@ -1,18 +1,24 @@
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
 import Posts from '../../src/js/components/Posts';
 import PostPreview from '../../src/js/components/PostPreview';
 
 describe('<Posts/>', () => {
   const posts = [
-    { id: 0, title: 'First Post' },
+    { id: 0, title: 'First Post', tags: ['js'] },
     { id: 1, title: 'Second Post' },
   ];
 
   it('renders list of posts', () => {
     const result = shallow(<Posts posts={posts} />);
     expect(result.find(PostPreview).length).to.eq(2);
+  });
+
+  it('passes tag to <PostPreview>', () => {
+    const result = mount(<Router><Posts tag="js" posts={posts} /></Router>);
+    expect(result.find('.tags__tag_active').text()).to.eq('js');
   });
 
   it('renders message when there are no posts', () => {
