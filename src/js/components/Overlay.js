b46d7abb9b6a548b177b8bcbf2eeedecd62574b5
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-18 17:29:56 +0300

    Make overlay navigation fill available space [skip ci]

--- src/js/components/Overlay.js
+++ src/js/components/Overlay.js
@@ -10,38 +10,40 @@ function Overlay({ isOpen, onClick }) {
   const secondaryClsName = 'overlay__nav__item overlay__nav__item_secondary';
   return (
     <section className={clsName}>
-      <header className="overlay__header">
-        <Logo className="overlay__logo">
-          <Link to="/" onClick={onClick}>Slava Pavlutin</Link>
-        </Logo>
-        <Icon
-          className="overlay__button"
-          name="times"
-          onClick={onClick}
-        />
-      </header>
-      <nav className="overlay__nav">
-        <ul>
-          <li className={primaryClsName}>
-            <Link to="/" onClick={onClick}>Home</Link>
-          </li>
-          <li className={primaryClsName}>
-            <Link to="/blog" onClick={onClick}>Blog</Link>
-          </li>
-          <li className={primaryClsName}>
-            <Link to="/projects" onClick={onClick}>Projects</Link>
-          </li>
-          <li className={secondaryClsName}>
-            <Link to="/about" onClick={onClick}>About</Link>
-          </li>
-          <li className={secondaryClsName}>
-            <Link to="/about#contact" onClick={onClick}>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <footer className="overlay__footer">
-        <SocialLinks />
-      </footer>
+      <div className="overlay__wrapper">
+        <header className="overlay__header">
+          <Logo className="overlay__logo">
+            <Link to="/" onClick={onClick}>Slava Pavlutin</Link>
+          </Logo>
+          <Icon
+            className="overlay__button"
+            name="times"
+            onClick={onClick}
+          />
+        </header>
+        <nav className="overlay__nav">
+          <ul>
+            <li className={primaryClsName}>
+              <Link to="/" onClick={onClick}>Home</Link>
+            </li>
+            <li className={primaryClsName}>
+              <Link to="/blog" onClick={onClick}>Blog</Link>
+            </li>
+            <li className={primaryClsName}>
+              <Link to="/projects" onClick={onClick}>Projects</Link>
+            </li>
+            <li className={secondaryClsName}>
+              <Link to="/about" onClick={onClick}>About</Link>
+            </li>
+            <li className={secondaryClsName}>
+              <Link to="/about#contact" onClick={onClick}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <footer className="overlay__footer">
+          <SocialLinks />
+        </footer>
+      </div>
     </section>
   );
 }
