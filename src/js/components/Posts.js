9cf3943d60db7d9ca0cfab248a9fb150faff658b
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-21 22:51:04 +0300

    Do not wrap posts not found message in .posts

--- src/js/components/Posts.js
+++ src/js/components/Posts.js
@@ -4,7 +4,7 @@ import PostPreview from './PostPreview';
 
 function Posts({ posts, tag }) {
   if (posts.length === 0) {
-    return <section className="posts"><p>No posts found.</p></section>;
+    return <p>No posts found.</p>;
   }
   return (
     <section className="posts">
